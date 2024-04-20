44cf97b06a16b485b5fba6a9357447ab77147b43
hotakus <ttowfive@gmail.com>
2024-04-20 12:17:14 +0800

    update: new

--- toolchain.cmake
+++ toolchain.cmake
@@ -4,3 +4,4 @@ set(CMAKE_C_COMPILER
 set(CMAKE_CXX_COMPILER
     /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-g++
     CACHE STRING "C++ compiler")
+set(CMAKE_C_LINKER_LAUNCHER /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-ld)
