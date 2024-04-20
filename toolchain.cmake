5398840b882ac424694e6e5aaccdebea7bf381fd
hotakus <ttowfive@gmail.com>
2024-04-20 12:28:19 +0800

    update: new

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,7 +1,15 @@
-set(CMAKE_C_COMPILER
-    /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-gcc
-    CACHE STRING "C compiler")
-set(CMAKE_CXX_COMPILER
-    /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-g++
-    CACHE STRING "C++ compiler")
-set(CMAKE_C_LINKER_LAUNCHER /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-ld)
+#set(CMAKE_C_COMPILER
+#    /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-gcc
+#    CACHE STRING "C compiler")
+#set(CMAKE_CXX_COMPILER
+#    /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-g++
+#    CACHE STRING "C++ compiler")
+#set(CMAKE_C_LINKER_LAUNCHER /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-ld)
+
+set(CMAKE_C_COMPILER    /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER  /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER  /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-gcc)
+set(CMAKE_AR            /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-ar)
+set(CMAKE_OBJCOPY       /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-objcopy)
+set(CMAKE_OBJDUMP       /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-objdump)
+set(SIZE                /root/dl/arm-none-eabi/arm-none-eabi/bin/arm-none-eabi-size)
