3410ec93f62992555b6cc33fc6de387e6d4d04aa
hotakus <ttowfive@gmail.com>
2024-04-21 22:57:16 +0800

    update: README.md

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 # 创建 build 目录
 mkdir -p build
-cd build
-# 运行 cmake 命令，传递参数
-cmake .. "$@"
-# 使用 make -j 编译
-make -j12
-cd ..
+(cd build || exit
+  # 运行 cmake 命令，传递参数
+  cmake .. "$@"
+  # 使用 make -j 编译
+  make -j12
+)
 
 # 检查文件是否存在并复制文件
 if [ -f "./bin/holog" ]; then
