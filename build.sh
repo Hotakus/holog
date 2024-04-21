bf8aa52ab115582422c293ed3ed5cafc3c25e03e
hotakus <ttowfive@gmail.com>
2024-04-21 19:11:50 +0800

    update: gitignore

--- build.sh
+++ build.sh
@@ -3,9 +3,15 @@
 # 创建 build 目录
 mkdir -p build
 cd build
-
 # 运行 cmake 命令，传递参数
 cmake .. "$@"
-
 # 使用 make -j 编译
 make -j12
+cd ..
+cp ./bin/holog .
+
+mkdir -p ./archive
+
+cp ./lib/libholog.a ./archive
+cp ./*.h ./archive
+
