676a029721f08cc0b255e2b9163e801cc2f9d242
hotakus <ttowfive@gmail.com>
2024-04-21 23:02:35 +0800

    update: README.md

--- build.sh
+++ build.sh
@@ -3,22 +3,22 @@
 # 创建 build 目录
 mkdir -p build
 
-
-  cd build
+(
+  cd build || exit
   # 运行 cmake 命令，传递参数
   cmake .. "$@"
   # 使用 make -j 编译
   make -j12
-
-  cd ..
+)
 
 # 检查文件是否存在并复制文件
 if [ -f "./bin/holog" ]; then
-    cp ./bin/holog .
+    cp ./bin/holog ./holog/
     echo "File copied successfully."
 fi
 
 mkdir -p ./holog
 cp ./lib/libholog.a ./holog
 cp ./*.h ./holog
+cp ./inc/*.h ./holog
 
